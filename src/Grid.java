88529db1a44ee481456085339077d09566604ee3
lucasfelber <lucas.felber@inode.at>
2021-10-14 22:41:24 +0200

    Feature 4: Implement functionality
    Added update() function to Cell
    
    Mofified Grid to support update
    
    Modified Sketch to run at lower framerates

--- src/Grid.java
+++ src/Grid.java
@@ -16,10 +16,20 @@ public class Grid {
                 cells[a][b] = new Cell(s, this, a, b);
             }
         }
+
+        cells[2][1].state = true;
+        cells[3][2].state = true;
+        cells[3][3].state = true;
+        cells[2][3].state = true;
+        cells[1][3].state = true;
     }
 
     public void update(){
-        //rect at ...
+        for(Cell[] cellArr : cells){
+            for(Cell cell : cellArr){
+                cell.update();
+            }
+        }
     }
 
     public void show(){
@@ -32,22 +42,10 @@ public class Grid {
             s.line(0, i, s.width, i);
         }
 
-        cells[0][0].state = true;
-        cells[0][1].state = true;
-        cells[0][2].state = true;
-        cells[1][0].state = true;
-        cells[1][1].state = true;
-        cells[1][2].state = true;
-        cells[2][0].state = true;
-        cells[2][1].state = true;
-        cells[2][2].state = true;
-
-        for(Cell[] cellArray : cells){
-            for(Cell cell : cellArray){
+        for(Cell[] cellArr : cells){
+            for(Cell cell : cellArr){
                 cell.show();
             }
         }
-
-        System.out.println(cells[0][0].getNumberOfNeighbours());
     }
 }
